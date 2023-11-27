b787e0933f7d3f1bf06a4a515f2867cccc8b35aa
Anderson O. Aristides <joyce.miranda@marisa.com.br>
2023-11-27 20:09:40 -0300

    feat: Criando o Microsservice de Gateway

---
+++ makersweb/msgateway/src/main/java/br/com/makersweb/msgateway/MSGatewayApplication.java
@@ -0,0 +1,28 @@
+package br.com.makersweb.msgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
+public class MSGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MSGatewayApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator routes(RouteLocatorBuilder builder) {
+		return builder
+				.routes()
+					.route(r -> r.path("/clientes/**").uri("lb://ms-clientes"))
+				.build();
+	}
+
+}
