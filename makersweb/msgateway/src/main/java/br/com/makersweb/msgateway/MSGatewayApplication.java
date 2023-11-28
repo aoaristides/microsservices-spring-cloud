5ba2491fafffd029ef3ddbad7ec20c15b02b6bee
Anderson O. Aristides <joyce.miranda@marisa.com.br>
2023-11-28 14:18:46 -0300

    feat: Add Rota MS Avaliador de Credito

--- makersweb/msgateway/src/main/java/br/com/makersweb/msgateway/MSGatewayApplication.java
+++ makersweb/msgateway/src/main/java/br/com/makersweb/msgateway/MSGatewayApplication.java
@@ -23,6 +23,7 @@ public class MSGatewayApplication {
 				.routes()
 					.route(r -> r.path("/clientes/**").uri("lb://ms-clientes"))
 					.route(r -> r.path("/cartoes/**").uri("lb://ms-cartoes"))
+					.route(r -> r.path("/avaliacoes-credito/**").uri("lb://ms-avaliador-credito"))
 				.build();
 	}
 
